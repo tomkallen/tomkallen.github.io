2a61953255ef549731ffbdb9de5fddda27cd7b24
tomkallen <spbeat@mail.ru>
2016-07-12 13:45:40 +0300

    backgrounds/skew/timer

--- about/main.js
+++ about/main.js
@@ -39,3 +39,12 @@ window.onload = function() {
     window.myPolarArea.update();
 
 };
+
+var span = document.getElementById("span");
+var counter = 0;
+var _array = ["stuff", "websites", "Javascript", "CSS", "webapps", "designs", "MEAN", "self awareness", "good habits", "sense of beauty", "fire"];
+setInterval(function() {
+    counter === _array.length - 1 ? counter = 0 : counter++;
+    span.innerHTML = _array[counter];
+
+}, 1000);
