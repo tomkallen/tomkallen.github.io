418476ce0749f2dade61aee2ab47b6d889ff217e
tomkallen <spbeat@mail.ru>
2016-07-12 12:52:06 +0300

    Who cares

--- about/main.js
+++ about/main.js
@@ -1,16 +1,14 @@
 var config = {
     data: {
-        datasets: [
-            {
-                data: [
-                    78, 34, 56, 79, 83, 64, 82, 28
-                ],
-                backgroundColor: [
-                    "#F7464A", "#46BFBD", "#FDB45C", "#949FB1", "#4D5360","#711baa", "#32492a", "#177ce3"
-                ],
-                label: 'My dataset'
-            }
-        ],
+        datasets: [{
+            data: [
+                78, 34, 56, 79, 83, 64, 82, 28
+            ],
+            backgroundColor: [
+                "#F7464A", "#46BFBD", "#FDB45C", "#949FB1", "#4D5360", "#711baa", "#32492a", "#177ce3"
+            ],
+            label: 'My dataset'
+        }],
         labels: ["JavaScript", "TypeScript", "Express", "HTML", "CSS", "Gulp, Webpack", "Git", "Angular"]
     },
     options: {
@@ -38,6 +36,6 @@ window.onload = function() {
     console.log(1);
     var ctx = document.getElementById("myChart").getContext("2d");
     window.myPolarArea = Chart.PolarArea(ctx, config);
-        window.myPolarArea.update();
+    window.myPolarArea.update();
 
 };
