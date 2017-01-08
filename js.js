98aa658b012647c4314fc20c7bbe1e88a5441027
tomkallen <spbeat@mail.ru>
2017-01-09 00:29:59 +0300

    yes

--- js.js
+++ js.js
@@ -2,6 +2,6 @@ var zz = document.getElementsByClassName("x")[0];
 
 window.addEventListener('scroll', function() {
     zz.classList[
-        window.scrollY > 140 ? 'add' : 'remove'
+        window.scrollY > 200 ? 'add' : 'remove'
     ]('scrolled');
 });
